907cb1acf179f8cfbe91ee2cd5b0a61e2f65097c
r8bywork <vanya.smolensky@gmail.com>
2023-12-19 11:46:55 +0300

    feat: prettier fix, change chart logic, normalview optimization and delete res.http file

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,47 +1,50 @@
-
-import {ChangeEvent, useMemo, useState} from 'react';
-import { Input, Table } from 'antd';
-import {CoinTable} from '../../interfaces.ts';
+import { Input, Table } from "antd";
+import { ChangeEvent, useMemo, useState } from "react";
+import { CoinTable } from "../../interfaces.ts";
 import { columnsConfig } from "./TableConfig.tsx";
 
 interface TableProps {
-    coins: CoinTable[];
-    changeSelectedCoin: (coinId: string) => void;
-    tags: {name: string; id: string}[];
+	coins: CoinTable[];
+	changeSelectedCoin: (coinId: string) => void;
+	tags: { name: string; id: string }[];
 }
 
-const CoinTable = ({ coins, changeSelectedCoin, tags}: TableProps) => {
-    const [searchText, setSearchText] = useState('');
-    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearchText(e.target.value);
-    };
+const CoinTable = ({ coins, changeSelectedCoin, tags }: TableProps) => {
+	const [searchText, setSearchText] = useState("");
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearchText(e.target.value);
+	};
 
-    const filteredCoins = useMemo(() => {
-        if (!searchText) return coins;
-        return coins.filter((coin) => coin.name.toLowerCase().includes(searchText.toLowerCase()));
-    }, [searchText, coins]);
+	const filteredCoins = useMemo(() => {
+		if (!searchText) return coins;
+		return coins.filter((coin) =>
+			coin.name.toLowerCase().includes(searchText.toLowerCase())
+		);
+	}, [searchText, coins]);
 
-    const parsedTags = useMemo(() => {
-        return tags?.map(tag => ({ text: tag.name, value: tag.id }));
-    }, [tags]);
+	const parsedTags = useMemo(() => {
+		return tags?.map((tag) => ({ text: tag.name, value: tag.id }));
+	}, [tags]);
 
-    return (
-        <div>
-            <Input
-                type="text"
-                placeholder="Search by name"
-                value={searchText}
-                onChange={handleSearch}
-            />
-            <Table rowKey={"id"}
-                   className={"cursor-pointer"}
-                   columns={columnsConfig(parsedTags)}
-                   dataSource={filteredCoins}
-                   onRow={(record) => ({
-                       onClick: () => changeSelectedCoin(record.id),
-                   })}/>
-        </div>
-    );
+	return (
+		<div>
+			<Input
+				type="text"
+				placeholder="Search by name"
+				value={searchText}
+				onChange={handleSearch}
+			/>
+			<Table
+				rowKey={"id"}
+				className={"cursor-pointer"}
+				columns={columnsConfig(parsedTags)}
+				dataSource={filteredCoins}
+				onRow={(record) => ({
+					onClick: () => changeSelectedCoin(record.id),
+				})}
+			/>
+		</div>
+	);
 };
 
 export default CoinTable;
--- src/components/Table/TableConfig.tsx
+++ src/components/Table/TableConfig.tsx
@@ -1,72 +1,78 @@
-import {CoinTable} from "../../interfaces.ts";
-import {Tag} from "antd";
+import { Tag } from "antd";
+import { CoinTable } from "../../interfaces.ts";
 
-export const columnsConfig = (parsedTags: { text: string; value: string }[]) => [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        sorter: (a: CoinTable, b: CoinTable) => a.name.localeCompare(b.name),
-    },
-    {
-        title: 'Symbol',
-        dataIndex: 'symbol',
-        key: 'symbol',
-        sorter: (a: CoinTable, b: CoinTable) => a.symbol.localeCompare(b.symbol),
-    },
-    {
-        title: 'Rank',
-        dataIndex: 'rank',
-        key: 'rank',
-        sorter: (a: CoinTable, b: CoinTable) => a.rank - b.rank,
-    },
-    {
-        title: 'Circulating Supply',
-        dataIndex: 'circulating_supply',
-        key: 'circulating_supply',
-        sorter: (a: CoinTable, b: CoinTable) => a.circulating_supply - b.circulating_supply,
-    },
-    {
-        title: 'Total Supply',
-        dataIndex: 'total_supply',
-        key: 'total_supply',
-        sorter: (a: CoinTable, b: CoinTable) => a.total_supply - b.total_supply,
-    },
-    {
-        title: 'Max Supply',
-        dataIndex: 'max_supply',
-        key: 'max_supply',
-        sorter: (a: CoinTable, b: CoinTable) => a.max_supply - b.max_supply,
-    },
-    {
-        title: 'Price (USD)',
-        dataIndex: ['quotes', 'USD', 'price'],
-        key: 'price_usd',
-        sorter: (a: CoinTable, b: CoinTable) => a.quotes.USD.price - b.quotes.USD.price,
-    },
-    {
-        title: 'Market Cap (USD)',
-        dataIndex: ['quotes', 'USD', 'market_cap'],
-        key: 'market_cap_usd',
-        sorter: (a: CoinTable, b: CoinTable) => a.quotes.USD.market_cap - b.quotes.USD.market_cap,
-    },
-    {
-        title: 'Tags',
-        dataIndex: 'tags',
-        key: 'tags',
-        render: (tags: { id: string, name: string }[]) => (
-            <>
-            {tags.map(tag => (
-                <Tag color="blue" key={tag.id}>
-                    {tag.name}
-                </Tag>
-            ))}
-            </>
-        ),
-        filters: parsedTags,
-        filterSearch: true,
-        onFilter: (value: string, record: CoinTable) =>
-            record.tags.some(tag => tag.id === value),
-        // onFilter: (value: string, record: CoinTable) => record.tags.some(tag => tag.id === value),
-    },
-]
+export const columnsConfig = (
+	parsedTags: { text: string; value: string }[]
+) => [
+	{
+		title: "Name",
+		dataIndex: "name",
+		key: "name",
+		sorter: (a: CoinTable, b: CoinTable) => a.name.localeCompare(b.name),
+	},
+	{
+		title: "Symbol",
+		dataIndex: "symbol",
+		key: "symbol",
+		sorter: (a: CoinTable, b: CoinTable) => a.symbol.localeCompare(b.symbol),
+	},
+	{
+		title: "Rank",
+		dataIndex: "rank",
+		key: "rank",
+		sorter: (a: CoinTable, b: CoinTable) => a.rank - b.rank,
+	},
+	{
+		title: "Circulating Supply",
+		dataIndex: "circulating_supply",
+		key: "circulating_supply",
+		sorter: (a: CoinTable, b: CoinTable) =>
+			a.circulating_supply - b.circulating_supply,
+	},
+	{
+		title: "Total Supply",
+		dataIndex: "total_supply",
+		key: "total_supply",
+		sorter: (a: CoinTable, b: CoinTable) => a.total_supply - b.total_supply,
+	},
+	{
+		title: "Max Supply",
+		dataIndex: "max_supply",
+		key: "max_supply",
+		sorter: (a: CoinTable, b: CoinTable) => a.max_supply - b.max_supply,
+	},
+	{
+		title: "Price (USD)",
+		dataIndex: ["quotes", "USD", "price"],
+		key: "price_usd",
+		sorter: (a: CoinTable, b: CoinTable) =>
+			a.quotes.USD.price - b.quotes.USD.price,
+	},
+	{
+		title: "Market Cap (USD)",
+		dataIndex: ["quotes", "USD", "market_cap"],
+		key: "market_cap_usd",
+		sorter: (a: CoinTable, b: CoinTable) =>
+			a.quotes.USD.market_cap - b.quotes.USD.market_cap,
+	},
+	{
+		title: "Tags",
+		dataIndex: "tags",
+		key: "tags",
+		render: (tags: { id: string; name: string }[]) => (
+			<>
+				{tags.map((tag) => (
+					<Tag color="blue" key={tag.id}>
+						{tag.name}
+					</Tag>
+				))}
+			</>
+		),
+		filters: [...parsedTags],
+		filterSearch: true,
+		// onFilter: (value: string, record: CoinTable) =>
+		//     record.tags.some(tag => tag.id === value),
+		// onFilter: (value: string, record: CoinTable) =>
+		// 	record.tags.some((tag) => tag.id === value),
+	},
+];
